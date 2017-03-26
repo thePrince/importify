784e3409b27cb17674a63f0fec76d70e33511354
Krish <ksigler1@gmail.com>
2017-03-25 22:20:07 -0700

    shut down authServer after run.sh terminates

--- run.sh
+++ run.sh
@@ -2,5 +2,13 @@
 #make this script executable: 'chmod +x run.sh'
 #must have python installed, and added to your PATH so that the 'python' command works
 
-python authServer.py
+#if the script is exited prematurely, make sure to shut down authServer
+control_c() {
+    kill $PID
+    exit
+}
+
+python authServer.py &
+PID=$!
 python songAdder.py
+kill $PID
